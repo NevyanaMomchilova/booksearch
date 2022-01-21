f6824e295a53287862f562d5cb1872d60fda10bc
Nevyana Momchilova <nevyana.dm@gmail.com>
2022-01-21 09:53:53 +0200

    quotes feature and hamburger icon removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,50 @@
 import "./Navbar.css";
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { BiBookHeart, BiMenu } from "react-icons/bi";
+import { BiBookHeart } from "react-icons/bi";
 import { RiHeartLine } from "react-icons/ri";
 
 const Navbar = ({ children, wishlistCounter }) => {
-    return (
-        <header className="navbar" >
-            <nav className="navbar-items-left" >
-                <BiMenu className="hamburger-icon" title="hamburger menu" />
-                <NavLink to="/booklist" >
-                    <div className="logo-container">
-                        <BiBookHeart className="logo" title="bookstore logo" />
-                        <p className="logo-text" >BOOK<br/>SEARCH</p>
-                    </div>    
-                </NavLink>
-                <ul className="navbar-menu" >
-                    <li>
-                        <NavLink to="/wishlist" className="navbar-menu-link" activeClassName="navbar-menu-link-active" >Wishlist</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/quotes" className="navbar-menu-link" activeClassName="navbar-menu-link-active" >Quotes</NavLink>
-                    </li>
-                </ul>
-            </nav>
-            <div className="navbar-items-right">
-                {/* Searct */}
-                { children }
-                {/* WishList */}
-                <NavLink to="/wishlist" >
-                    <div className="wishlist-container" >
-                        <div className="wishlist-bubble" >
-                            <p className="wishlist-number" >
-                                {wishlistCounter}
-                            </p>
-                        </div>
-                        <RiHeartLine className="wishlist-icon" />
-                    </div>
-                </NavLink>
-            </div>
-        </header>
-    );
+	return (
+		<header className="navbar">
+			<nav className="navbar-items-left">
+				<NavLink to="/booklist">
+					<div className="logo-container">
+						<BiBookHeart className="logo" title="bookstore logo" />
+						<p className="logo-text">
+							BOOK
+							<br />
+							SEARCH
+						</p>
+					</div>
+				</NavLink>
+				<ul className="navbar-menu">
+					<li>
+						<NavLink
+							to="/wishlist"
+							className="navbar-menu-link"
+							activeClassName="navbar-menu-link-active"
+						>
+							Wishlist
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+			<div className="navbar-items-right">
+				{/* Searct */}
+				{children}
+				{/* WishList */}
+				<NavLink to="/wishlist">
+					<div className="wishlist-container">
+						<div className="wishlist-bubble">
+							<p className="wishlist-number">{wishlistCounter}</p>
+						</div>
+						<RiHeartLine className="wishlist-icon" />
+					</div>
+				</NavLink>
+			</div>
+		</header>
+	);
 };
 
-export default Navbar; 
+export default Navbar;
